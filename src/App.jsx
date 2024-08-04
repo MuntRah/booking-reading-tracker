78dd89925bfcf7696a65fc038151525d1aab4fd3
MuntRah <me.alrahim.02@gmail.com>
2024-08-04 14:27:51 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+// src/App.jsx
+import "./App.css";
+
 const App = () => {
+  const items = [
+    { name: "harrypotter", readed: true },
+    { name: "jumami", readed: true },
+    { name: "strabox", readed: false },
+    { name: "eggwar", readed: true },
+  ];
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>books List</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li
+            key={index}
+            
+            className={item.readed ? "completed" : "not-completed"}
+          >
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
 
-export default App
+export default App;
